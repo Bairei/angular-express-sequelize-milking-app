591de2e249dd3f620ad03b65446889d59b970b6b
Bairei <baireikawagishi@gmail.com>
2018-01-04 09:13:20 +0100

    add DELETE routing method to cow route, PUT and DELETE methods to milking route, edit PUT and POST methods in cow route

--- routes/cow.js
+++ routes/cow.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Cow = require('../models/Cow');
+const Milking = require('../models/Milking');
 
 // POST cow
 router.post('/', (req, res, next) => {
-  Cow.create({
-    name: req.body.name
-  }).then(cow => {
+  Cow.create(req.body).then(cow => {
     res.json(cow);
   }, err => {
     if (err) next(err);
@@ -31,10 +30,11 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
+// update cow
 router.put('/:id', (req, res, next) => {
   Cow.findById(req.params.id).then(cow => {
     if (req.body.name) {
-      cow.update({'name': req.body.name}).then(cow => {
+      cow.update(req.body).then(cow => {
         console.log('updated cow: id = ' + req.params.id);
       }, err => {
         if (err) return next(err);
@@ -43,6 +43,21 @@ router.put('/:id', (req, res, next) => {
     res.json(cow);
   }, err => {
     if (err) next(err);
+  });
+});
+
+// delete cow and every milking associated with it
+router.delete('/:id', (req, res, next) => {
+  Cow.findById(req.params.id).then(cow => {
+    if (cow === null) {
+      return res.status(404).send('Unable to find cow with specific id');
+    }
+    let ret = cow;
+    Milking.destroy({where: {cowId: cow.id}}).then(() => {
+      cow.destroy({force: true}).then(() => {
+        return res.json(ret);
+      });
+    });
   });
 });
 
--- routes/milking.js
+++ routes/milking.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const Cow = require('../models/Cow');
 const Milking = require('../models/Milking');
 
+// GET all milkings, along with cows assosiated with them
 router.get('/', (req, res, next) => {
   Milking.findAll().then(milkings => {
     let updatedMilkings = [];
     milkings.forEach((el, ind) => {
       updatedMilkings.push(Cow.findById(el.cowId).then(cow => {
+        // append 'cow' to current Milking object
         el.dataValues['cow'] = cow.dataValues;
         return el;
       }));
@@ -21,6 +23,7 @@ router.get('/', (req, res, next) => {
   });
 });
 
+// GET specific milking
 router.get('/:id', (req, res, next) => {
   Milking.findById(req.params.id).then(milking => {
     res.json(milking);
@@ -29,6 +32,7 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
+// Create new milking
 router.post('/', (req, res, next) => {
   if (!req.body.cowId) {
     return res.status(400).send("bad request, you need to provide cowID to object's body!");
@@ -37,15 +41,40 @@ router.post('/', (req, res, next) => {
     if (cow === null) {
       return res.status(404).send(`No such cow with id = ${req.body.cowId}`);
     }
-    Milking.create({
-      date: req.body.date,
-      cowId: req.body.cowId,
-      litres: req.body.litres
-    }).then(milking => {
+    Milking.create(req.body).then(milking => {
       cow.addMilking(milking).then(() => {
         res.json(milking);
       });
     });
+  });
+});
+
+// Update milking
+router.put('/:id', (req, res, next) => {
+  Milking.findById(req.params.id).then(milking => {
+    Cow.findById(req.body.cowId).then(cow => {
+      if (cow === null) {
+        return res.status(400).send('Wrong cowId!');
+      }
+      milking.update(req.body).then(updatedMilking => {
+        return res.json(updatedMilking);
+      });
+    });
+  }, err => {
+    if (err) next(err);
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  Milking.findById(req.params.id).then(milking => {
+    let deletedMilking = milking;
+    milking.destroy().then(() => {
+      return res.json(deletedMilking);
+    }, err => {
+      if (err) return next(err);
+    });
+  }, err => {
+    if (err) return next(err);
   });
 });
 
